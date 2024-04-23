c6bfe85f51d189fe984ea1ae2610b4fa8eca9f1a
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-04-23 22:49:46 +0700

    feat:change TAX_ALLOWANCECONFIG > TAX_DEDUCTIONCONFIG

--- init.sql
+++ init.sql
@@ -1,9 +1,11 @@
-CREATE TABLE IF NOT EXISTS TAX_ALLOWANCECONFIG (
+CREATE TABLE IF NOT EXISTS TAX_DEDUCTIONCONFIG (
   id SERIAL PRIMARY KEY,
-  allowance_type VARCHAR(30) NOT NULL,
-  allowance_amount DECIMAL(10,2) NOT NULL
+  deduction_type VARCHAR(30) NOT NULL,
+  deduction_amount DECIMAL(10,2) NOT NULL,
+  deduction_min DECIMAL(10,2) NOT NULL,
+  deduction_max DECIMAL(10,2) NOT NULL
 );
 
-INSERT INTO TAX_ALLOWANCECONFIG (allowance_type, allowance_amount) VALUES
- ('personal', 60000),
- ('k-receipt', 50000);
+INSERT INTO TAX_DEDUCTIONCONFIG (deduction_type, deduction_amount, deduction_min, deduction_max) VALUES
+ ('personal', 60000, 10000, 100000),
+ ('k-receipt', 50000, 0, 100000);
