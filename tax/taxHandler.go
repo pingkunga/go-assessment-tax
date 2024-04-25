f1e750744137668d4550fae794b6d53e76c6bd8a
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-04-26 06:57:14 +0700

    feat: story-6 upload csv file form multiplart

--- tax/taxHandler.go
+++ tax/taxHandler.go
@@ -2,7 +2,11 @@ package tax
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -20,4 +24,55 @@ func CalculationsHandler(c echo.Context) error {
 
 	TaxResponse := CalculateTax(taxRequest)
 	return c.JSON(http.StatusOK, TaxResponse)
+}
+
+func BatchCalculationsHandler(c echo.Context) error {
+	// Multipart form
+	//-----------
+	// Read file
+	//-----------
+
+	// Source
+	file, err := c.FormFile("taxFile")
+	if err != nil {
+		return err
+	}
+
+	//https://stackoverflow.com/questions/48349927/how-to-write-a-directory-with-a-file-in-golang
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Destination
+	//taxes.csv
+	CheckBatchTxPath("Uploads")
+	file.Filename = generateFileName(file.Filename)
+	dst, err := os.Create(filepath.Join("Uploads", file.Filename))
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, "File uploaded successfully")
+}
+
+func CheckBatchTxPath(p string) {
+	if _, err := os.Stat(p); os.IsNotExist(err) {
+		os.Mkdir(p, 0770)
+	}
+}
+
+func fileNameWithoutExt(fileName string) string {
+	return fileName[:len(fileName)-len(filepath.Ext(fileName))]
+}
+
+func generateFileName(fileName string) string {
+	return fmt.Sprintf("%s-%d%s", fileNameWithoutExt(fileName), time.Now().Unix(), filepath.Ext(fileName))
 }
